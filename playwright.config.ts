3bd67c1c7955d6ec9a513cc3b16756de9dfbca8a
Heiko Fanieng <heiko@fanieng.com>
2024-11-05 15:37:54 +0100

    Merge branch 'master' of https://github.com/devsfiae/la-mediterranea.astro

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 testDir: './tests',
 use: {
-    baseURL: 'http://localhost:3000', // Passe den Port an deinen Astro-Server an
+    baseURL: 'http://localhost:4321', // Passe den Port an deinen Astro-Server an
     browserName: 'chromium', // Du kannst auch firefox oder webkit verwenden
 },
 webServer: {
     command: 'npm run dev', // Startet den Astro-Server für Tests
-    port: 3000,
+    port: 4321,
     reuseExistingServer: !process.env.CI,
 },
 });
