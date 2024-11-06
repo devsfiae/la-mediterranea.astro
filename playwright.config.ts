2bf60e0e58050d5bf9c3497763c27c06af26bd4a
Heiko Fanieng <heiko@fanieng.com>
2024-11-06 17:38:59 +0100

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-testDir: './tests',
-reporter: 'html',
-use: {
-    baseURL: 'http://localhost:4321', // Passe den Port an deinen Astro-Server an
-    browserName: 'chromium', // Du kannst auch firefox oder webkit verwenden
-},
-webServer: {
-    command: 'npm run dev', // Startet den Astro-Server für Tests
-    port: 4321,
-    reuseExistingServer: !process.env.CI,
-},
+    testDir: './tests',
+    reporter: 'html',
+    use: {
+        baseURL: 'http://localhost:4321', // Passe den Port an deinen Astro-Server an
+        browserName: 'chromium', // Nutzt die Chromium-Engine, ähnlich wie Safari
+    },
+    webServer: {
+        command: 'npm run dev', // Startet den Astro-Server für Tests
+        port: 4321,
+        reuseExistingServer: !process.env.CI,
+    },
 });
---
+++ tests/la_mediterranea.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:4321/');
+  await page.locator('#content-wrapper label span').click();
+  await page.locator('#content-wrapper label span').click();
+  await page.getByLabel('food page').click();
+  await page.getByLabel('drinks page').click();
+  await page.getByText('start food drinks reserve events contact about us light').click();
+  await page.getByLabel('event page').click();
+  await page.getByLabel('contact page').click();
+  await page.getByText(': Not found Path: /Contact').click();
+  await page.getByLabel('about page').click();
+  await page.getByRole('heading', { name: 'Heiko Fanieng' }).click();
+  await page.getByRole('heading', { name: 'Hiba Al Anssari' }).click();
+  await page.getByRole('heading', { name: 'Irina Zittlau' }).click();
+  await page.getByRole('heading', { name: 'Puya Khandany' }).click();
+  await page.getByRole('img', { name: 'Heiko Fanieng' }).click();
+  await page.getByRole('img', { name: 'Hiba Al Anssari' }).click();
+  await page.getByRole('img', { name: 'Irina Zittlau' }).click();
+  await page.getByRole('img', { name: 'Puya Khandany' }).click();
+  await page.getByRole('img', { name: 'Holger Kramer' }).click();
+  await page.getByText('known for his inspiring').click();
+  await page.getByText('Motivator, Mentor & pace-').click();
+});
