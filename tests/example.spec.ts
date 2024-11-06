174ae25845ec9db96cede473a0cbf577bf056d88
Heiko Fanieng <heiko@fanieng.com>
2024-11-06 09:48:04 +0100

    updates

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-// tests/example.spec.ts
-import { test, expect } from '@playwright/test';
-
-test('Homepage has correct title', async ({ page }) => {
-await page.goto('/');
-await expect(page).toHaveTitle(/start @ La Mediterranea/);
-});
